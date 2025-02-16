f2d201e4988d7ee7ec617eaed361498067320cb9
salomao-neto <salomao.cruz.15@gmail.com>
2025-02-16 06:27:34 -0300

    chore: added import rules to linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,34 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tsEslint from "typescript-eslint";
+import { configs as tsEslintConfig } from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslintPluginImportX from "eslint-plugin-import-x";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  ...tsEslint.configs.recommended,
+  ...tsEslintConfig.recommended,
   eslintPluginPrettierRecommended,
+  eslintPluginImportX.flatConfigs.recommended,
+  eslintPluginImportX.flatConfigs.typescript,
   { ignores: ["app-example/*", "node_modules/*", "./expo-env.d.ts"] },
+  {
+    settings: {
+      "import-x/parser": {
+        "@typescript-eslint/parser": ["**/*.{ts,tsx}"],
+      },
+      "import-x/resolver": {
+        typescript: true,
+      },
+    },
+    rules: {
+      "import-x/no-cycle": [
+        "error",
+        {
+          maxDepth: 3,
+        },
+      ],
+    },
+  },
 ];
