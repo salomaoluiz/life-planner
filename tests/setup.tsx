a8a59846625cdbeb43fe7c0c0057a9ce24c403e7
salomao-neto <salomao.cruz.15@gmail.com>
2025-03-04 16:36:14 -0300

    chore: updated jest and tsconfig

--- tests/setup.tsx
+++ tests/setup.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+// Mock Envs
+
+process.env = {
+  NODE_ENV: "test",
+  EXPO_PUBLIC_SUPABASE_URL: "EXPO_PUBLIC_SUPABASE_URL",
+  EXPO_PUBLIC_SUPABASE_ANON_KEY: "EXPO_PUBLIC_SUPABASE_ANON_KEY",
+};
+
 jest.mock("react-native-paper", () => {
   const View = jest.requireActual("react-native").View;
   return {
@@ -21,4 +29,18 @@ jest.mock("@react-native-google-signin/google-signin", () => ({
     signOut: jest.fn(),
     hasPlayServices: jest.fn(),
   },
+}));
+
+jest.mock("@react-native-async-storage/async-storage", () => jest.fn());
+
+jest.mock("@supabase/supabase-js", () => ({
+  createClient: jest.fn().mockReturnValue({
+    auth: {
+      signInWithIdToken: jest.fn(),
+      signInWithOAuth: jest.fn(),
+      setSession: jest.fn(),
+      signOut: jest.fn(),
+      getUser: jest.fn(),
+    },
+  }),
 }));
