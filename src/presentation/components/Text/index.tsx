725c3d6a3e366e3dcfb56b33bc3883cbaf0cb628
salomao-neto <salomao.cruz.15@gmail.com>
2025-03-09 16:35:00 -0300

    chore: added customStyles to Text component

--- src/presentation/components/Text/index.tsx
+++ src/presentation/components/Text/index.tsx
@@ -1,44 +1,68 @@
 import { Text as PaperText } from "react-native-paper";
 import { getStyles } from "./styles";
 import { TextMode } from "./types";
+import getCustomStyles, { CustomStyles } from "./styles/customStyles";
 
 export interface TextProps {
   value: string;
   testID: string;
+  customStyles?: CustomStyles;
 }
 
 function TextBase(props: TextProps & { mode: TextMode }) {
-  const { testID, value, mode } = props;
+  const { testID, value, mode, customStyles } = props;
 
   const styles = getStyles();
-
+  const extraStyles = getCustomStyles(customStyles);
   return (
-    <PaperText variant={mode} testID={testID} style={styles[mode]}>
+    <PaperText
+      variant={mode}
+      testID={testID}
+      style={[styles[mode], extraStyles]}
+    >
       {value}
     </PaperText>
   );
 }
 
+/*
+    @fontSize - 48
+ */
 function Display(props: TextProps) {
   return <TextBase mode={TextMode.Display} {...props} />;
 }
 
+/*
+    @fontSize - 24
+ */
 function Headline(props: TextProps) {
   return <TextBase mode={TextMode.Headline} {...props} />;
 }
 
+/*
+    @fontSize - 20
+ */
 function Title(props: TextProps) {
   return <TextBase mode={TextMode.Title} {...props} />;
 }
 
+/*
+    @fontSize - 16
+ */
 function Body(props: TextProps) {
   return <TextBase mode={TextMode.Body} {...props} />;
 }
 
+/*
+    @fontSize - 14
+ */
 function Label(props: TextProps) {
   return <TextBase mode={TextMode.Label} {...props} />;
 }
 
+/*
+    @fontSize - 12
+ */
 function Caption(props: TextProps) {
   return <TextBase mode={TextMode.Caption} {...props} />;
 }
