b7ffa602c0123be40365cc8256a8b4fce490828f
salomao-neto <salomao.cruz.15@gmail.com>
2025-03-09 15:43:27 -0300

    chore: added react query

---
+++ src/infrastructure/provider/index.test.tsx
@@ -0,0 +1,14 @@
+import { setup, spies, screen } from "./mocks/index.mocks";
+
+it("SHOULD render the provider", () => {
+  setup();
+
+  expect(screen.getByTestId("fetcher-provider")).toBeTruthy();
+  expect(screen.getByTestId("infrastructure-children")).toBeTruthy();
+});
+
+it("SHOULD initialize GoogleOAuth", () => {
+  setup();
+
+  expect(spies.initializeGoogleOAuth).toHaveBeenCalledTimes(1);
+});
