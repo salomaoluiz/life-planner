b4500030ed3bb3cd7f827eaf4863b485d7116857
salomao-neto <salomao.cruz.15@gmail.com>
2025-03-11 20:33:42 -0300

    tests: fix tests

--- src/providers/index.test.tsx
+++ src/providers/index.test.tsx
@@ -1,5 +1,7 @@
 import * as applicationProviders from "@application/providers";
 import * as presentationProviders from "@presentation/providers";
+import * as infrastructureProvider from "@infrastructure/provider";
+
 import * as loaderProvider from "./loader";
 import { View } from "react-native";
 import { render, screen } from "@tests";
@@ -9,6 +11,12 @@ jest
   .spyOn(presentationProviders, "default")
   .mockImplementation(({ children }) => (
     <View testID={"presentation-providers"}>{children}</View>
+  ));
+
+jest
+  .spyOn(infrastructureProvider, "default")
+  .mockImplementation(({ children }) => (
+    <View testID={"infrastructure-providers"}>{children}</View>
   ));
 
 jest
@@ -25,18 +33,14 @@ jest
 
 const Children = () => <View testID={"children-element"} />;
 
-const setup = () =>
-  render(
-    <GlobalProviders>
-      <Children />
-    </GlobalProviders>,
-  );
+const setup = () => render(<Children />, { wrapper: GlobalProviders });
 
 it('SHOULD render "PresentationProviders", "ApplicationProviders" and "LoaderProvider"', () => {
   setup();
 
   expect(screen.getByTestId("presentation-providers")).toBeOnTheScreen();
   expect(screen.getByTestId("application-providers")).toBeOnTheScreen();
+  expect(screen.getByTestId("infrastructure-providers")).toBeOnTheScreen();
   expect(screen.getByTestId("loader-provider")).toBeOnTheScreen();
   expect(screen.getByTestId("children-element")).toBeOnTheScreen();
   expect(screen.toJSON()).toMatchSnapshot();
