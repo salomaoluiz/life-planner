bed3a40e8a0608dd9d3d6cd8b5bce9554623ee57
salomao-neto <salomao.cruz.15@gmail.com>
2025-03-15 15:15:54 -0300

    chore: added family and family member to datasource list

--- src/data/datasource/data/index.ts
+++ src/data/datasource/data/index.ts
@@ -1,9 +1,13 @@
 import * as auth from "./auth";
+import * as family from "./family";
+import * as familyMember from "./family-member";
 import * as user from "./user";
 
 const listDatasources = {
   ...auth,
   ...user,
+  ...family,
+  ...familyMember,
 };
 
 export { listDatasources };
