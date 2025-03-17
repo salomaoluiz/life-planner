2febd2bd67471f9bf1bea23d6fc7c523ef5e3cad
salomao-neto <salomao.cruz.15@gmail.com>
2025-03-17 20:18:59 -0300

    chore: created the skeleton

---
+++ src/presentation/components/Skeleton/Circle.tsx
@@ -0,0 +1,37 @@
+import ContentLoader, { Circle } from "react-content-loader/native";
+
+import { useTheme } from "@presentation/theme";
+
+interface Props {
+  size: number;
+}
+
+function CircleSkeleton(props: Props) {
+  const { theme } = useTheme();
+  return (
+    <ContentLoader
+      backgroundColor={theme.colors.onSurface}
+      foregroundColor={theme.colors.onSurfaceVariant}
+      height={props.size * 2}
+      speed={1}
+      style={{
+        height: props.size,
+        position: "absolute",
+        width: props.size,
+        zIndex: 1,
+      }}
+      testID={"skeleton-loader"}
+      viewBox={`0 0 ${props.size * 2} ${props.size * 2}`}
+      width={props.size * 2}
+    >
+      <Circle
+        cx={props.size}
+        cy={props.size}
+        r={props.size}
+        testID={"skeleton-circle"}
+      />
+    </ContentLoader>
+  );
+}
+
+export default CircleSkeleton;
