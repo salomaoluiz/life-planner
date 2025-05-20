a92d87366a0a9b856d16efbd2123ad7b90bc020d
salomao-neto <salomao.cruz.15@gmail.com>
2025-05-20 18:55:22 -0300

    feat: created financial categories cache key

--- src/infrastructure/cache/types.ts
+++ src/infrastructure/cache/types.ts
@@ -2,6 +2,7 @@ export enum CacheStringKeys {
   CACHE_CONFIGS_DATA = "@cache_configs_data",
   CACHE_FAMILIES_DATA = "@cache_families_data",
   CACHE_FAMILY_MEMBERS_DATA = "@cache_family_members_data",
+  CACHE_FINANCIAL_CATEGORY_DATA = "@cache_financial_category_data",
   CACHE_FINANCIAL_TRANSACTION_DATA = "@cache_financial_transaction_data",
   CACHE_STOCK_DATA = "@cache_stock_data",
   CACHE_USER_BY_ID_DATA = "@cache_user_by_id_data",
